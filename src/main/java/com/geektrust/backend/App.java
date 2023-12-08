1342f69b3e52823b86e6eec2932ad8d99c10e799
CrioUser <crio-user@criodo.com>
2023-12-09 02:11:57 +0530

    ME_GEEKTRUST_BACKEND

--- src/main/java/com/geektrust/backend/App.java
+++ src/main/java/com/geektrust/backend/App.java
@@ -15,22 +15,23 @@ public class App {
 
 	// To run the application  ./gradlew run --args="INPUT_FILE=sample_input/input1.txt"
 	public static void main(String[] args) {
-		System.out.println("Welcome to Geektrust Backend Challenge!");
+		// System.out.println("Welcome to Geektrust Backend Challenge!");
 		List<String> commandLineArgs = new LinkedList<>(Arrays.asList(args));
-        String expectedSequence = "sample_input/input1.txt";
-        String actualSequence = commandLineArgs.stream()
-                .map(a -> a.split("=")[1])
-                .collect(Collectors.joining("$"));
-        if(expectedSequence.equals(actualSequence)){
+        // String expectedSequence = "sample_input/input1.txt";
+        // String actualSequence = commandLineArgs.stream()
+        //         .map(a -> a.split("=")[1])
+        //         .collect(Collectors.joining("$"));
+        // if(expectedSequence.equals(actualSequence)){
             run(commandLineArgs);
-        }
+        // }
 	}
 	public static void run(List<String> commandLineArgs) {
 		// Complete the final logic to run the complete program.
 		ApplicationConfig applicationConfig = new ApplicationConfig();
 		CommandInvoker commandInvoker = applicationConfig.getCommandInvoker();
-		String inputFile = commandLineArgs.get(0).split("=")[1];
-		commandLineArgs.remove(0);
+		String inputFile = commandLineArgs.get(0);
+		// .split("=")[1];
+		// commandLineArgs.remove(0);
 		try(BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
 			String line = reader.readLine();
 			while (line != null) {
