b6f0680ca34c59da3103d81e074c32bf64e87dba
CrioUser <crio-user@criodo.com>
2024-01-20 12:55:23 +0530

    magic number removed

--- src/main/java/com/geektrust/backend/config/ApplicationConfig.java
+++ src/main/java/com/geektrust/backend/config/ApplicationConfig.java
@@ -15,6 +15,7 @@ import com.geektrust.backend.service.CourseService;
 import com.geektrust.backend.service.RegistrationSevice;
 import com.geektrust.backend.service.serviceImpl.CourseServiceImpl;
 import com.geektrust.backend.service.serviceImpl.RegistrationServiceImpl;
+import com.geektrust.backend.utils.Constant;
 
 public class ApplicationConfig {
 
@@ -33,10 +34,10 @@ public class ApplicationConfig {
     private final CommandInvoker commandInvoker = new CommandInvoker();
 
     public CommandInvoker getCommandInvoker(){
-        commandInvoker.register("ADD-COURSE-OFFERING", addCourseCommand);
-        commandInvoker.register("REGISTER", registerCommand);
-        commandInvoker.register("ALLOT", allotCommand);
-        commandInvoker.register("CANCEL", cancelCommand);
+        commandInvoker.register(Constant.ADD_COURSE_OFFERING, addCourseCommand);
+        commandInvoker.register(Constant.REGISTER, registerCommand);
+        commandInvoker.register(Constant.ALLOT, allotCommand);
+        commandInvoker.register(Constant.CANCEL, cancelCommand);
 
         return commandInvoker;
     }
--- src/main/java/com/geektrust/backend/utils/Constant.java
+++ src/main/java/com/geektrust/backend/utils/Constant.java
@@ -10,6 +10,11 @@ public class Constant {
     public static final String INPUT_DATA_ERROR_MESSAGE = "INPUT_DATA_ERROR";
     public static final String COURSE_FULL_ERROR_MESSAGE="COURSE_FULL_ERROR";
     public static final String COURSE_ALREADY_ALLOTED="COURSE_ALREADY_ALLOTED";
+    public static final String ADD_COURSE_OFFERING="ADD-COURSE-OFFERING";
+    public static final String REGISTER="REGISTER";
+    public static final String ALLOT="ALLOT";
+    public static final String CANCEL="CANCEL";
+
 
     public static final int EXPECTED_ARGS_ADD_COURSE = 6;
     public static final int EXPECTED_ARGS_REGISTER_COURSE = 3;
