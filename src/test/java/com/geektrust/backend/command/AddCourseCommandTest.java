b63add33d9a659d9e6d06279d66343bb403ac1d9
CrioUser <crio-user@criodo.com>
2024-01-20 15:22:43 +0530

    all CommandTest added

--- src/test/java/com/geektrust/backend/command/AddCourseCommandTest.java
+++ src/test/java/com/geektrust/backend/command/AddCourseCommandTest.java
@@ -8,20 +8,83 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.geektrust.backend.dto.CourseDto;
 import com.geektrust.backend.service.CourseService;
 import com.geektrust.backend.utils.Constant;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
 @ExtendWith(MockitoExtension.class)
 public class AddCourseCommandTest {
+
+    
+    String command;
+    String courseName;
+    String instructor;
+    String date;
+    String minEmployee;
+    String maxEmployee;
+    List<String> correctTokens;
+    List<String> wrongTokens;
+
+
+    //ADD-COURSE-OFFERING JAVA JAMES 15062022 1 2
+    @BeforeEach
+    void setup(){
+        command="ADD-COURSE-OFFERING";
+        courseName="JAVA";
+        instructor="JAMES";
+        date="15062022";
+        minEmployee="1";
+        maxEmployee="2";
+        correctTokens=new ArrayList<>(List.of(command,courseName,instructor,date,minEmployee,maxEmployee));
+        wrongTokens=new ArrayList<>(List.of(command,courseName));
+    }
+
+    @Test
+    @DisplayName("calling addCourseCommand method should return with courseId")
+    void addCourseCommandShouldReturnCourseId() {
+        //arrrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        String expectedCourseId="OFFERING-"+courseName+"-"+instructor+"";
+        when(courseService.createCourse(any(CourseDto.class))).thenReturn(expectedCourseId);
+        //act
+        addCourseCommand.execute(correctTokens);
+        //assert
+        assertEquals(expectedCourseId, outputStream.toString().trim());
+        verify(courseService).createCourse(any(CourseDto.class));
+        //Clean up
+        System.setOut(System.out);
+    }
+
+    @Test
+    @DisplayName("calling addCourseCommand method should return exception InvalidInputException with message INPUT_DATA_ERROR")
+    void addCourseCommandShouldReturnInput_Data_Error(){
+        //arrrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        String expectedCourseId=Constant.INPUT_DATA_ERROR_MESSAGE;
+        //act
+        addCourseCommand.execute(wrongTokens);
+        //assert
+        assertEquals(expectedCourseId, outputStream.toString().trim());
+        //Clean up
+        System.setOut(System.out);
+    }
+
     private CourseService courseService;
     private AddCourseCommand addCourseCommand;
 
@@ -102,65 +165,5 @@ public class AddCourseCommandTest {
         // Clean up
         System.setOut(System.out);
     }
-//     private final PrintStream standardOut = System.out;
-//     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-//     String command;
-//     String courseName;
-//     String instructor;
-//     String date;
-//     String minEmployee;
-//     String maxEmployee;
-//     List<String> correctTokens;
-//     List<String> wrongTokens;
-
-
-//     @Mock
-//     CourseService courseServiceMock;
-//     @InjectMocks
-//     AddCourseCommand addCourseCommand;
-
-//     //ADD-COURSE-OFFERING JAVA JAMES 15062022 1 2
-//     @BeforeEach
-//     void setup(){
-//         System.setOut(new PrintStream(outputStreamCaptor));
-
-//         command="ADD-COURSE-OFFERING";
-//         courseName="JAVA";
-//         instructor="JAMES";
-//         date="15062022";
-//         minEmployee="1";
-//         maxEmployee="2";
-//         correctTokens=new ArrayList<>(List.of(command,courseName,instructor,date,minEmployee,maxEmployee));
-//         wrongTokens=new ArrayList<>(List.of(command,courseName));
-//     }
-
-//     @Test
-//     @DisplayName("calling addCourseCommand method should return with courseId")
-//     void addCourseCommandShouldReturnCourseId() {
-//         //arrrange
-//         String expectedCourseId="OFFERING-"+courseName+"-"+instructor+"";
-//         when(courseServiceMock.createCourse(any(CourseDto.class))).thenReturn(expectedCourseId);
-//         //act
-//         addCourseCommand.execute(correctTokens);
-//         //assert
-//         assertEquals(expectedCourseId, outputStreamCaptor.toString().trim());
-//         verify(courseServiceMock).createCourse(any(CourseDto.class));
-//     }
-
-//     @Test
-//     @DisplayName("calling addCourseCommand method should return exception InvalidInputException with message INPUT_DATA_ERROR")
-//     void addCourseCommandShouldReturnInput_Data_Error(){
-//         //arrrange
-//         String expectedCourseId=Constant.INPUT_DATA_ERROR_MESSAGE;
-//         //act
-//         addCourseCommand.execute(wrongTokens);
-//         //assert
-//         assertEquals(expectedCourseId, outputStreamCaptor.toString().trim());
-//     }
-
-//     @AfterEach
-//     public void tearDown() {
-//         System.setOut(standardOut);
-//     }
 }
--- src/test/java/com/geektrust/backend/command/AllotCommandTest.java
+++ src/test/java/com/geektrust/backend/command/AllotCommandTest.java
@@ -1,87 +1,88 @@
-// package com.geektrust.backend.command;
+package com.geektrust.backend.command;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.mockito.Mockito.verify;
-// import static org.mockito.Mockito.when;
-// import java.io.ByteArrayOutputStream;
-// import java.io.PrintStream;
-// import java.util.ArrayList;
-// import java.util.List;
-// import com.geektrust.backend.dto.AllotResponse;
-// import com.geektrust.backend.service.CourseService;
-// import com.geektrust.backend.utils.Constant;
-// import org.junit.jupiter.api.AfterEach;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import com.geektrust.backend.dto.AllotResponse;
+import com.geektrust.backend.service.CourseService;
+import com.geektrust.backend.utils.Constant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// @ExtendWith(MockitoExtension.class)
-// public class AllotCommandTest {
-//     private final PrintStream standardOut = System.out;
-//     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+@ExtendWith(MockitoExtension.class)
+public class AllotCommandTest {
 
-//     @Mock
-//     CourseService courseServiceMock;
+    @Mock
+    CourseService courseServiceMock;
 
-//     @InjectMocks
-//     AllotCommand allotCommand;
+    @InjectMocks
+    AllotCommand allotCommand;
 
-//     String emailId;
-//     String courseName;
-//     String instructor;
-//     String date;
-//     String status;
-//     String regId;
-//     String command;
-//     String courseId;
-//     List<String> correctTokens;
-//     List<String> wrongTokens;
-//     List<AllotResponse> allotResponse;
+    String emailId;
+    String courseName;
+    String instructor;
+    String date;
+    String status;
+    String regId;
+    String command;
+    String courseId;
+    List<String> correctTokens;
+    List<String> wrongTokens;
+    List<AllotResponse> allotResponse;
 
-//     @BeforeEach
-//     void setup(){
-//         System.setOut(new PrintStream(outputStreamCaptor));
-//         command="ALLOT";
-//         courseId="OFFERING-JAVA-JAMES";
-//         instructor="JAMES";date="15062022";status="CONFIRMED";regId="REG-COURSE-ANDY-JAVA";courseName="JAVA";
-//         emailId="ANDY@GMAIL.COM";
-//         correctTokens=new ArrayList<>(List.of(command,courseId));
-//         wrongTokens=new ArrayList<>(List.of(command));
+    @BeforeEach
+    void setup(){
+        command="ALLOT";
+        courseId="OFFERING-JAVA-JAMES";
+        instructor="JAMES";date="15062022";status="CONFIRMED";regId="REG-COURSE-ANDY-JAVA";courseName="JAVA";
+        emailId="ANDY@GMAIL.COM";
+        correctTokens=new ArrayList<>(List.of(command,courseId));
+        wrongTokens=new ArrayList<>(List.of(command));
 
-//         allotResponse=new ArrayList<>(List.of(new AllotResponse(regId, emailId, courseId, courseName, instructor, date, status)));
+        allotResponse=new ArrayList<>(List.of(new AllotResponse(regId, emailId, courseId, courseName, instructor, date, status)));
 
-//     }
+    }
 
-//     @Test
-//     @DisplayName("allotCommand should print registered employee")
-//     void allotCommandShouldPrintRegisterEmployee(){
-//         //arrange
-//         String expectedAllotResponse=allotResponse.get(0).toString();
-//         when(courseServiceMock.allot(correctTokens.get(1))).thenReturn(allotResponse);
-//         //act
-//         allotCommand.execute(correctTokens);
-//         //assert
-//         assertEquals(expectedAllotResponse, outputStreamCaptor.toString().trim());
-//         verify(courseServiceMock).allot(correctTokens.get(1));
-//     }
+    @Test
+    @DisplayName("allotCommand should print registered employee")
+    void allotCommandShouldPrintRegisterEmployee(){
+        //arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        String expectedAllotResponse=allotResponse.get(0).toString();
+        when(courseServiceMock.allot(correctTokens.get(1))).thenReturn(allotResponse);
+        //act
+        allotCommand.execute(correctTokens);
+        //assert
+        assertEquals(expectedAllotResponse, outputStream.toString().trim());
+        verify(courseServiceMock).allot(correctTokens.get(1));
+        //Clean up
+        System.setOut(System.out);
+    }
 
-//     @Test
-//     @DisplayName("allotCommand should return exception InvalidInputException with message INPUT_DATA_ERROR")
-//     void allotCommandWithWrongTokensSizeShouldPrintInput_Data_Erro(){
-//         //arrange
-//         String expectedAllotResponse=Constant.INPUT_DATA_ERROR_MESSAGE;
-//         //act
-//         allotCommand.execute(wrongTokens);
-//         //assert
-//         assertEquals(expectedAllotResponse, outputStreamCaptor.toString().trim());
-//     }
-
-//     @AfterEach
-//     public void tearDown() {
-//         System.setOut(standardOut);
-//     }
-// }
+    @Test
+    @DisplayName("allotCommand should return exception InvalidInputException with message INPUT_DATA_ERROR")
+    void allotCommandWithWrongTokensSizeShouldPrintInput_Data_Erro(){
+        //arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        String expectedAllotResponse=Constant.INPUT_DATA_ERROR_MESSAGE;
+        //act
+        allotCommand.execute(wrongTokens);
+        //assert
+        assertEquals(expectedAllotResponse, outputStream.toString().trim());
+        //Clean up
+        System.setOut(System.out);
+    }
+}
