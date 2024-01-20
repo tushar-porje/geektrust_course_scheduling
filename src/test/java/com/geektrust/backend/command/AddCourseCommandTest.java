f5707021c883af989d7b80eac6d196becc69b787
CrioUser <crio-user@criodo.com>
2024-01-20 15:33:28 +0530

    edit CommandTest added

--- src/test/java/com/geektrust/backend/command/AddCourseCommandTest.java
+++ src/test/java/com/geektrust/backend/command/AddCourseCommandTest.java
@@ -8,14 +8,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.geektrust.backend.dto.CourseDto;
 import com.geektrust.backend.service.CourseService;
 import com.geektrust.backend.utils.Constant;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -102,63 +100,6 @@ public class AddCourseCommandTest {
         verify(courseService, never()).createCourse(any(CourseDto.class));
 
         // Clean up
-        System.setOut(System.out);
-    }
-
-    String command;
-    String courseName;
-    String instructor;
-    String date;
-    String minEmployee;
-    String maxEmployee;
-    List<String> correctTokens;
-    List<String> wrongTokens;
-
-
-    //ADD-COURSE-OFFERING JAVA JAMES 15062022 1 2
-    @BeforeEach
-    void setup(){
-        command="ADD-COURSE-OFFERING";
-        courseName="JAVA";
-        instructor="JAMES";
-        date="15062022";
-        minEmployee="1";
-        maxEmployee="2";
-        correctTokens=new ArrayList<>(List.of(command,courseName,instructor,date,minEmployee,maxEmployee));
-        wrongTokens=new ArrayList<>(List.of(command,courseName));
-    }
-
-    @Test
-    @DisplayName("calling addCourseCommand method should return with courseId")
-    void addCourseCommandShouldReturnCourseId() {
-        //arrrange
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
-        String expectedCourseId="OFFERING-"+courseName+"-"+instructor+"";
-        when(courseService.createCourse(any(CourseDto.class))).thenReturn(expectedCourseId);
-        //act
-        addCourseCommand.execute(correctTokens);
-        //assert
-        assertEquals(expectedCourseId, outputStream.toString().trim());
-        verify(courseService).createCourse(any(CourseDto.class));
-        //Clean up
-        System.setOut(System.out);
-    }
-
-    @Test
-    @DisplayName("calling addCourseCommand method should return exception InvalidInputException with message INPUT_DATA_ERROR")
-    void addCourseCommandShouldReturnInput_Data_Error(){
-        //arrrange
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
-        String expectedCourseId=Constant.INPUT_DATA_ERROR_MESSAGE;
-        //act
-        addCourseCommand.execute(wrongTokens);
-        //assert
-        assertEquals(expectedCourseId, outputStream.toString().trim());
-        //Clean up
         System.setOut(System.out);
     }
 }
